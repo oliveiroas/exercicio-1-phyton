d13266cdc978af7a152155b03eaf0935fcc1254d
oliveiroas <0000111979318xsp@al.educacao.sp.gov.br>
2025-05-06 12:01:40 -0300

    cadastrar  aluno

--- exercicio.05.py
+++ exercicio.05.py
@@ -1,13 +1,16 @@
-def adicionar_aluno(nome, email, serie):
+def adicionar_aluno(nome, email, serie, nota01, nota02, nota03,):
     dados_alunos = []  
     aluno = {
         "nome": nome,
         "email": email,
-        "serie": serie
+        "serie": serie,
+        "nota": [nota01, nota02, nota03]
     }
     dados_alunos.append(aluno)
     return dados_alunos
 
-print(adicionar_aluno("Vinicius", "viniciuscabulosao@gmail.com", "2b série"))
+print(adicionar_aluno("Vinicius", "viniciuscabulosao@gmail.com", "2b série", 10, 10, 10))
+
+
 
 
